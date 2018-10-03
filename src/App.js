39b70c73d30699c4bb169a97a083fdeddb720754
Alex Jivov <alexjivov@gwu.edu>
2018-10-03 19:48:17 -0400

    headrr successfully added + relevant mdl imports to index.js file. base styled header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+//import minified CSS
+import './Assets/css/default.min.css';
+import {Layout, Header} from 'react-mdl';
 
 // // REACT ROUTER - Allows navigation of different webpages - npm react-router-dom
 // import { Link } from 'react-router-dom';
 
 
-//import minified CSS
-import './Assets/css/default.min.css';
+
+
+
+/////////////////
+////COMPONENTS////
+/////////////////
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Click Game </h1>
+        <div style={{ height: '300px', position: 'relative' }}>
+          <Layout fixedHeader>
+            <Header className="header-color" title={<h1 style={{ textDecoration: 'none', color: 'white'}} to="/">Click Game</h1>} scroll>
+            </Header>
+          </Layout>
+          </div>
+    
+
       </div>
     );
   }
