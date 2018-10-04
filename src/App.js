1bed99dac903048c08f7ac8141d0760fe7e2d174
Alex Jivov <alexjivov@gwu.edu>
2018-10-03 20:15:02 -0400

    footer moved into app.js - no logic means making a component is redundant. styled and inserted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 //import minified CSS
 import './Assets/css/default.min.css';
-import {Layout, Header} from 'react-mdl';
+import {Layout, Header, Footer} from 'react-mdl';
 
 // // REACT ROUTER - Allows navigation of different webpages - npm react-router-dom
 // import { Link } from 'react-router-dom';
@@ -21,13 +21,21 @@ class App extends Component {
       <div className="App">
         <div style={{ height: '300px', position: 'relative' }}>
           <Layout fixedHeader>
-            <Header className="header-color" title={<h1 style={{ textDecoration: 'none', color: 'white'}} to="/">Click Game</h1>} scroll>
+            <Header className="header-color" title={<h1 style={{ textDecoration: 'none', color: 'white',}} to="/"><i class="fas fa-brain" style={{marginRight: '10px'}}></i>Click Game</h1>} scroll>
             </Header>
           </Layout>
-          </div>
+        </div>
+    
     
 
-      </div>
+
+
+        <Footer className ="footer-color">
+                <h4 style={{color: '#fff',textAlign:'center',fontSize:'15px'}}>University of Toronto Coding Bootcamp & Alexander Jivov 2018</h4>
+            
+            </Footer>
+ 
+    </div>
     );
   }
 }
