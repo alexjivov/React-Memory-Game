31ae139fe1f318bce3027c3c6cfbef34a22f85fa
Alex Jivov <alexjivov@gwu.edu>
2018-10-09 20:57:27 -0400

    created card component (empty) and added in top jumbotron - needs additional styling for space

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 //import minified CSS
 import './Assets/css/default.min.css';
-import {Layout, Header, Footer} from 'react-mdl';
+import { Layout, Header, Footer } from 'react-mdl';
 
 // // REACT ROUTER - Allows navigation of different webpages - npm react-router-dom
 // import { Link } from 'react-router-dom';
@@ -21,23 +21,34 @@ class App extends Component {
       <div className="App">
         <div style={{ height: '300px', position: 'relative' }}>
           <Layout fixedHeader>
-            <Header className="header-color" title={<h1 style={{ textDecoration: 'none', color: 'white',}} to="/"><i class="fas fa-brain" style={{marginRight: '10px'}}></i>Click Game</h1>} scroll>
+            <Header
+              className="header-color" title={<h1 style={{ textDecoration: 'none', color: 'white', }} to="/"><i class="fas fa-brain" style={{ marginRight: '10px' }}></i>Click Game</h1>} scroll>
+              <h4>Click an Image to Begin!</h4>
+              <h4> Score:    | Top Score:    </h4>
             </Header>
+
+            <div className="jumbotron">
+            <h1 class="display-4">Test Your Memory!</h1>
+            <p class="lead">Click on an image to earn points, but don't click on any more than once!</p>          
+          </div>
           </Layout>
+          
+
+          
+         
+          
+
+
+
+          <Footer className="footer-color">
+            <h4 style={{ color: '#fff', textAlign: 'center', fontSize: '15px' }}>University of Toronto Coding Bootcamp & Alexander Jivov 2018</h4>
+
+          </Footer>
+
         </div>
+        </div>
+        );
+      }
+    }
     
-    
-
-
-
-        <Footer className ="footer-color">
-                <h4 style={{color: '#fff',textAlign:'center',fontSize:'15px'}}>University of Toronto Coding Bootcamp & Alexander Jivov 2018</h4>
-            
-            </Footer>
- 
-    </div>
-    );
-  }
-}
-
-export default App;
+    export default App;
---
+++ src/Components/Cards/Card.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Card, CardTitle, Grid, Cell } from 'react-mdl';
+
+
+class Cards extends Component {
+    render() {
+        return {
+
+
+
+
+
+
+        }
+    }
+}
+
+export default Cards;
