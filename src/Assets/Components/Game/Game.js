4dc9ac46f5dbfa8adb8923a4d2fd3bd4000327f7
Alex Jivov <alexjivov@gwu.edu>
2018-10-11 14:50:31 -0400

    created Game.js component to handle memory game. built out initialCards function - untested

---
+++ src/Assets/Components/Game/Game.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import Cards from './Cards/Cards';
+
+function InitialCards() {
+    return [
+        {value: 2, matched: false, flipped: false},
+        {value:4, matched: false, flipped: false},
+        {value:1, matched: false, flipped: false},
+        {value:1, matched: false, flipped: false},
+        {value:3, matched: false, flipped: false},
+        {value:4, matched: false, flipped: false},
+        {value:2, matched: false, flipped: false},
+        {value:3, matched: false, flipped: false},
+
+    ];
+}
+
+
+
+class Game extends Component {
+    render() {
+        return (    
+
+
+
+
+    )
+    }
+}
+
+export default Game; 
