deb08a48df3fba04ed99ae46c5134b348c37293c
Alex Jivov <alexjivov@gwu.edu>
2018-10-29 20:32:13 -0400

    ready to deploy - minor css changes needed

--- src/Assets/Components/Wrapper/Wrapper.js
+++ src/Assets/Components/Wrapper/Wrapper.js
@@ -2,7 +2,7 @@ import React from "react";
 
 // set properties for the wrapper around the images and return everything (props.children)
 const Wrapper = props => (
-  <div className="container wrapper">{props.children}</div>
+  <div className="container-wrapper">{props.children}</div>
 );
 
 export default Wrapper;
